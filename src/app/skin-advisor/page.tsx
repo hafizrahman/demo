4028177c17b8631e00abab414c11c4b5cc70e790
Hafiz Rahman <hfzrhmn@gmail.com>
2025-04-28 16:40:05 +0700

    Smaller design fixes.

--- src/app/skin-advisor/page.tsx
+++ src/app/skin-advisor/page.tsx
@@ -2,7 +2,7 @@ import SkinAdvisorApp from "@/components/client/SkinAdvisorApp";
 
 export default function Page() {
     return (
-        <div id="wrapper" className="bg-gradient-to-br from-fuchsia-200 to-red-100 h-screen p-12">
+        <div id="wrapper" className="bg-gradient-to-br from-fuchsia-200 to-red-100 p-12 h-screen overflow-y-auto">
                 <SkinAdvisorApp />
         </div>
     );
--- src/components/client/ColorProfileSection.tsx
+++ src/components/client/ColorProfileSection.tsx
@@ -27,7 +27,7 @@ export default function ColorProfileSection({ colorProfile }: ColorProfileSectio
 
             <h2 className="text-xl font-bold mt-8">Recommended color palette</h2>
 
-            <div className="grid grid-cols-2 sm:grid-cols-3 gap-2 mt-4 bg-gray-100 rounded-lg p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-2 mt-4 bg-gray-100 rounded-lg p-4">
                 {colorProfile.colors.map((color: ColorInfo, index: number) => (
                     <div key={index} className="flex flex-col items-center">
                         <div
