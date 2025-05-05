7f1c4e0576500d52b1566d11209290b58762cc3d
Hafiz Rahman <hfzrhmn@gmail.com>
2025-05-05 12:06:56 +0700

    Exclude skin-advisor demo page from auth requirement for now.

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,8 +12,9 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
+     * - /skin-advisor (does not require auth for now)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|skin-advisor|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
